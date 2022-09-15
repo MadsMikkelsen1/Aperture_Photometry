99b1b06dfaaae1b5dd3533e0be32c39016fc7da9
Mads Mikkelsen <Mads@mik86.dk>
2022-09-15 16:22:31 +0200

    test

---
+++ Aperture Photometry.py
@@ -0,0 +1,20 @@
+# APERTURE PHOTOMETRY #
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from IPython.display import set_matplotlib_formats
+set_matplotlib_formats('pdf', 'svg')
+
+plt.rcParams['figure.figsize'] = (15,10)
+plt.rc("axes", labelsize=18)
+plt.rc("xtick", labelsize=16, top=True, direction="in")
+plt.rc("ytick", labelsize=16, right=True, direction="in")
+plt.rc("axes", titlesize=22)
+plt.rc("legend", fontsize=16, loc="upper left")
+plt.rc("figure", figsize=(10, 7))
+sns.set_style("darkgrid")
+sns.set_context("paper")
+sns.set(font_scale=1.4)
+mycolors = ['#C188F7','#F79288','#7FB806','#F59B18']
+sns.set_palette("Set2") 
+print("Hej")
